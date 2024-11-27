137fe42ecbc692703d128b5a3f608a786c57d0aa
Ronaldo618 <ronaldoodesouzasilva18@gmail.com>
2024-11-27 14:52:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'cultura Pop',
+    'qual o simbolo do Super Man?',
+    'Um S'
+)
+
+criaCartao(
+    'Geografia',
+    'Que pais é conhecido como a capital do amor?',
+    'A cidade de Paris na França'
+)
+
+criaCartao(
+    'Matematica',
+    '27+3-30?',
+    '0'
+)
+
+criaCartao(
+    'Sla',
+    'Recado ',
+    'Cade minha NOTAAAA!!!'
+)
